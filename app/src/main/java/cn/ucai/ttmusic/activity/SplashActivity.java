323aef57de90d3e0181abea2f24ae46ea8481bd3
Yurnero <765773633qq.com>
2016-12-06 17:00:21 +0800

    加入动态权限申请；适配有底部导航栏的手机

--- app/src/main/java/cn/ucai/ttmusic/activity/SplashActivity.java
+++ app/src/main/java/cn/ucai/ttmusic/activity/SplashActivity.java
@@ -1,9 +1,13 @@
 package cn.ucai.ttmusic.activity;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 
 import java.util.List;
 
@@ -15,6 +19,12 @@ import cn.ucai.ttmusic.utils.MusicUtil;
 public class SplashActivity extends BaseActivity {
 
     Context mContext;
+
+    static final int REQUEST_EXTERNAL_STORAGE = 1;
+    static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
     static final long DELAY_TIME = 2000;
 
     @Override
@@ -27,6 +37,29 @@ public class SplashActivity extends BaseActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        //申请读取文件的权限
+        int permission = ActivityCompat.checkSelfPermission(mContext, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            goToMainActivity();
+        } else {
+            ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_EXTERNAL_STORAGE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //申请成功后，2秒跳转至主界面
+                goToMainActivity();
+            } else {
+                finish();
+            }
+        }
+    }
+
+    private void goToMainActivity() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -41,5 +74,4 @@ public class SplashActivity extends BaseActivity {
             }
         }, DELAY_TIME);
     }
-
 }
